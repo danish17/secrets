b4eaca3c97af07034d338cae8b400311a1ae4663
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-09 01:39:22 +0530

    feat: store secret message as ciphertext

--- src/secret/secret.controller.ts
+++ src/secret/secret.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -7,18 +8,72 @@ import {
   Post,
 } from '@nestjs/common';
 import { ApiParam, ApiTags } from '@nestjs/swagger';
+import { SecretService } from './secret.service';
+import { SecretEntity } from './entities/secret.entity';
+import { encryptMessage, generateHash, generateSalt } from './utils/crypt';
+import { CreateSecretDto } from './dto/secret.dto';
+import * as moment from 'moment';
 
 @ApiTags('secret')
 @Controller('secret')
 export class SecretController {
+  constructor(private readonly secretService: SecretService) {}
+
+  @ApiParam({
+    name: 'uri',
+    type: String,
+    description: 'Unique Identifier of the secret.',
+  })
+  @Get('/:uri')
+  getSecretByUri(@Param('uri') uri): string {
+    return `${uri}`;
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'ID of the secret.',
+  })
   @Get('/:id')
-  getSecretById(@Param('id') id): string {
-    return `${id} Hello`;
+  async getSecretById(@Param('id') id): Promise<SecretEntity | HttpStatus> {
+    const findResult = await this.secretService.findOneById(id);
+
+    if (!findResult) {
+      return HttpStatus.NOT_FOUND;
+    }
+
+    return findResult;
   }
 
   @Post()
-  createSecret(): string {
-    return 'Secret created';
+  async createSecret(@Body() data: CreateSecretDto) {
+    const { validFor, viewsAllowed, passphrase, secret: secretContent } = data;
+    const salt = generateSalt();
+    const hashedPassphrase = generateHash(passphrase, salt);
+    const createdAt = moment();
+    const expiresAt = createdAt.add(validFor, 'h');
+    const uri = await this.secretService.generateUri();
+
+    // Store secret message as ciphertext.
+    const { cipherText, iv } = encryptMessage(
+      'aes-192-cbc',
+      secretContent,
+      passphrase,
+    );
+
+    const secret = await this.secretService.createSecret({
+      uri,
+      views_allowed: viewsAllowed,
+      views_left: viewsAllowed,
+      created_at: createdAt.toDate(),
+      expires_at: expiresAt.toDate(),
+      passphrase: hashedPassphrase,
+      salt,
+      iv,
+      secret: cipherText,
+    });
+
+    return secret;
   }
 
   @ApiParam({

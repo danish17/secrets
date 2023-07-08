3bdefce7aba3f752856b8e1afdfdc537098fdb81
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-09 00:25:08 +0530

    feat: update dto

--- src/secret/dto/secret.dto.ts
+++ src/secret/dto/secret.dto.ts
@@ -1,9 +1,6 @@
 export class CreateSecretDto {
-  uri: string;
-  created_at: Date;
-  expires_at: Date;
-  views_allowed: number;
-  views_left: number;
+  validFor: number;
+  viewsAllowed: number;
   passphrase: string;
   secret: string;
 }

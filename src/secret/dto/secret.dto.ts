e3e43cb04e11377f5da618b4b44c09b45a330773
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-09 03:07:42 +0530

    feat: add validation

--- src/secret/dto/secret.dto.ts
+++ src/secret/dto/secret.dto.ts
@@ -1,6 +1,19 @@
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateSecretDto {
+  @IsInt()
+  @IsNotEmpty()
   validFor: number;
+
+  @IsInt()
+  @IsNotEmpty()
   viewsAllowed: number;
+
+  @IsString()
+  @IsNotEmpty()
   passphrase: string;
+
+  @IsString()
+  @IsNotEmpty()
   secret: string;
 }

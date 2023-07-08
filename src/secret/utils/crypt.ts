e01a2a337e26994bbdd62a110ed2906d06e12903
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-09 01:37:59 +0530

    feat: add encryption/decryption utils

--- src/secret/utils/crypt.ts
+++ src/secret/utils/crypt.ts
@@ -15,7 +15,7 @@ export const generateSalt = (): string => {
  */
 export const generateHash = (plainPassword: string, salt: string): string => {
   const hash = crypto
-    .pbkdf2Sync(plainPassword, salt, 1000, 64, `sha512`)
+    .pbkdf2Sync(plainPassword, salt, 1000, 64, `sha1`)
     .toString(`hex`);
 
   return hash;
@@ -30,4 +30,56 @@ export const generateHash = (plainPassword: string, salt: string): string => {
  */
 export const validatePassword = (attemptedPlainPassword, salt, hash) => {
   return hash === generateHash(attemptedPlainPassword, salt);
+};
+
+/**
+ * Encrypts a message.
+ * @param algorithm cipher to be used (AES-192-CBC)
+ * @param content plain text
+ * @param passphrase raw passphrase
+ * @returns Object
+ */
+export const encryptMessage = (algorithm, content, passphrase) => {
+  const iv = Buffer.alloc(16, 0);
+  const key = crypto.scryptSync(passphrase, 'GfG', 24);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+  let cipherText = '';
+
+  cipher.on('readable', () => {
+    let chunk;
+    while (null !== (chunk = cipher.read())) {
+      cipherText += chunk.toString('base64');
+    }
+  });
+
+  cipher.write(content);
+  cipher.end();
+
+  return {
+    cipherText,
+    iv: iv.toString(),
+  };
+};
+
+/**
+ * Decrypts a message.
+ * @param algorithm cipher to be used (AES-192-CBC)
+ * @param cipherText encrypted text
+ * @param passphrase raw passphrase
+ * @param iv initialization vector
+ * @returns decrypted text
+ */
+export const decryptMessage = (algorithm, cipherText, passphrase, iv) => {
+  const key = crypto.scryptSync(passphrase, 'GfG', 24);
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+  let decryptedText = decipher.update(
+    Buffer.from(cipherText, 'base64').toString('hex'),
+    'hex',
+    'utf-8',
+  );
+  decryptedText += decipher.final('utf-8');
+
+  return decryptedText;
 };

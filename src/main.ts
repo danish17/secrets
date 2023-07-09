f0a3fb6b9e6d4f86d6776b6abe8be34d0facb051
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-09 21:24:09 +0530

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
   await app.listen(3000);
   Logger.log(`*** Application is being served on: ${await app.getUrl()} ***`);
 }
